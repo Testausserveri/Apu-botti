971bf5e66ccc5301bee75fefc13b228094c6859e
sandflare7532 <niubnet@gmail.com>
2022-04-22 21:38:51 +0300

    Rewrote the translator, because it looked horrible.

--- src/utils/translate.rs
+++ src/utils/translate.rs
@@ -1,19 +1,33 @@
-// The Translator for "Apu-bot".
-// Writen by n1ubi.
-//
-// Example:
-//  Miten printtaan muuttujan C?
-//             vvvv
-//  How do I print a variable in C
-//
-// To dos marked with block comment, notes/explanations marked with //
-//
+//! Turn Finnish sentences into English sentences using a list of words.
+//!
+//! Should panic if the rule file is bad, but doesn't for some reason.
+//! Doesn't panic if the message is bad.
+//!
+//! # Rule file format
+//! [key] = [value] (newline)
+//! where key is the Finnish word and value is the English word.
+//!
+//! # Example
+//! "tervehdys" = "hello" (\n)
+//!
+//! # Note
+//! - There should only be lower-case characters. Saving a few nanosenconds at run-time.
+//! - The rule file shouldn't contain any mistakes.
+//!   (Writing a linter for that later if this project comes back alive.)
+//! - This thing doesn't understand contexts
+//!   Example of that would be "terve".
+//!   "terve" could mean "healthy" or "hello" depending on the context.
 
-use std::env; // For "command-line" arguments.
+struct Translator {
+    english_sentence: Vec<String>,
+    sentence: Vec<String>,
+    rules: String,
+}
 
-
-pub fn parse(input: String) -> (String) {
-    /*let client = reqwest::Client::new();
+/// Use a thirdparty translator to translate the message.
+/// Not ready. Maybe later.
+pub fn translate_online(_input: Vec<String>) -> Result<Vec<String>, String> {
+/*let client = reqwest::Client::new();
     let body = client.post("https://translate.yandex.net/api/v1/tr.json/translate")
         .body(r#"text=help&options=4"#)
         .send()
@@ -23,71 +37,138 @@ pub fn parse(input: String) -> (String) {
         .await
         .unwrap();
     println!("{}",body);*/
-
-    let mut output = String::from("");
-    // Hopefully you can read the code.
-    //let con = "input.contains"; // I tried this, but didn't work.
-    if input != "" {
-        // The actual translating part.
-        if input.contains("miten") || input.contains("kuinka")              {output.push_str("how do I ");}
-        if input.contains("print")                                          {output.push_str("print ");}
-        if input.contains("muuttuja") || input.contains("var")              // Too long.
-            || input.contains("variable")                                           {output.push_str("variable");}
-        if input.contains("string") || input.contains("teksti")             {output.push_str("string");}
-        //if input.contains("") || input.contains("")              {output.push_str("");}
-        if input.contains("jos") || input.contains("if")                    {output.push_str("if");}
-        if input.contains("tai") || input.contains("else")                  {output.push_str("else");}
-        /* TO DO: if input contains unknown word, output.push_str(unknownword) */
-        // If contains language name, add it to the output. (To make the search result more accurate.)
-        if input.contains("js") || input.contains("javascript")             {output.push_str(" JavaScript");}
-        if input.contains("py") || input.contains("python")                 {output.push_str(" Python");}
-        if input.contains("c")                                              {output.push_str(" C");}
-        if input.contains("cpp") || input.contains("C++")                   {output.push_str(" C++");}
-        if input.contains("c-sharp") || input.contains("c#")                {output.push_str(" C#");}
-        if input.contains("java") || input.contains("jar")                  {output.push_str(" Java");}
-        if input.contains("rs") || input.contains("rust")                   {output.push_str(" Rust");}
-        if input.contains("ps") || input.contains("powershell")             {output.push_str(" Powershell");}
-        if input.contains("sh") || input.contains("bash")                   {output.push_str(" Bash");}
-        if input.contains("bat") || input.contains("batch")                 {output.push_str(" Batch");}
-        if input.contains("kotlin")                                         {output.push_str(" Kotlin");}
-        if input.contains("swift")                                          {output.push_str(" Swift");}
-        if input.contains("html")                                           {output.push_str(" HTML");}
-        if input.contains("css")                                            {output.push_str(" CSS");}
-        if input.contains("perl")                                           {output.push_str(" Perl");}
-        if input.contains("haskell")                                        {output.push_str(" Haskell");}
-        if input.contains("sql")                                            {output.push_str(" SQL");}
-        if input.contains("ts") || input.contains("typescript")             {output.push_str(" TypeScript");}
-        if input.contains("lua") || input.contains("roblox script")         {output.push_str(" Lua");}
-        if input.contains("php")                                            {output.push_str(" PHP");}
-        if input.contains("objectivec") || input.contains("objc")           {output.push_str(" Obj-C");}
-        if input.contains("assembly") || input.contains("asm")              {output.push_str(" Assembly");}
-        if input.contains("autoit")                                         {output.push_str(" Autoit");}
-        if input.contains("coffeescript")                                   {output.push_str(" CoffeeScript");}
-        if input.contains("vbs") || input.contains("visual basic")          {output.push_str(" VBS");}
-        if input.contains("dart")                                           {output.push_str(" Dart");}
-        if input.contains("xml")                                            {output.push_str(" XML");}
-        if input.contains("pascal")                                         {output.push_str(" Pascal");}
-        if input.contains("ruby")                                           {output.push_str(" Ruby");}
-
-        // Output => link   I can ignore all this vvvv       output vvvvvvv
-        // Replace " " with "+". ( https://www.google.com/search?q=google+search )
-        // + = %2B, / = %2F
-        output = output.replace("+", "%2B").replace("/", "%2F").replace(" ", "+");
-    }
-    else {
-        /* return an error or something. */
-        print!("An error occured.");
-    }
-    output
+    Ok(Vec::new())
 }
 
-/*pub fn main()
-{
-    let args: Vec<String> = env::args().collect();
-    let arg = &args[1];
-    // Program assumes it's going to get only one argument.
+/// Translate the text from Finnish to English according to the rule file.
+/// The error should be sent by the Discord bot.
+///
+/// Note: Won't work as intended
+/// if the one who wrote the message didn't add spaces
+/// or the spaces are in weird places.
+pub fn translate_rule_based(input: Vec<String>, rule_file: String) -> Result<Vec<String>, String> {
+    // Add error handling here
+    let r = std::fs::read_to_string(&rule_file).unwrap();
 
-    let input = arg.to_string().replace("-", " ").to_lowercase();
+    let mut d = Vec::new();
 
-    parse(input);
-}*/
+    // Remove unwanted characters.
+    for i in 0..input.len() {
+        let mut temp = input[i].clone();
+
+        if input[i].contains(".") {
+            temp = input[i].replace(".", "")
+        }
+
+        if input[i].contains("?") {
+            temp = input[i].replace("?", "")
+        }
+
+        if input[i].contains("!") {
+            temp = input[i].replace("!", "")
+        }
+
+        d.push(temp.to_lowercase())
+    }
+
+    let mut t = Translator {
+        english_sentence: Vec::new(),
+        sentence: d,
+        rules: r
+    };
+
+    let s = t.sentence.clone();
+
+    for word in s {
+        match t.translate(word.to_string()) {
+            // Discord bot should send this message below in some form.
+            Err(e) => return Err(format!("An error '{}' occured when attempted to translate() '{}'.", e, word)),
+            Ok(_) => {}
+        }
+    }
+
+    Ok(t.english_sentence)
+}
+
+impl Translator {
+    /// Attempts to find `key` from rule file
+    /// and returns the index of last character of the word
+    /// or possibly an error.
+    fn find(&self, key: &String) -> Result<usize, ()> {
+        if key.is_empty() {
+            return Err(())
+        }
+
+        for (i, c) in self.rules.chars().enumerate() {
+            if c.to_string() == &key[0..1] {
+                if i + key.len() > self.rules.len() {
+                    // No more file left
+                    // which means that `key` was not found.
+                    return Err(())
+                }
+
+                if key.as_str() == &self.rules[i..i + key.len()] {
+                    return Ok(i + key.len())
+                }
+            }
+        }
+        // `key` was not found
+        return Err(())
+    }
+    /// Parse a value.
+    // I tried to do this with a macro, but it didn't work :(
+    fn parse_value(&mut self, index: usize) {
+        let mut temp = String::new();
+        let mut value_parsed = false;
+
+        for i in index..self.rules.len() {
+            match &self.rules[i..i + 1] {
+                " " => {}
+                "\n" => if value_parsed { break } else {
+                    panic!("New line before value? Index: {}", index);
+                }
+                // Probably not a good idea to add this all-covering thing
+                // when only looking for characters, but it's fine.
+                c => {
+                    temp.push_str(c);
+                    value_parsed = true
+                }
+            }
+        }
+
+        self.english_sentence.push(temp);
+    }
+
+    /// Reads a `key` from the config file and returns the English equvalient of it
+    /// or if an error occured, returns Err(()).
+    /// If [key] = "Hello", returns "Hello".
+    fn translate(&mut self, key: String) -> Result<(), String> {
+        if key.is_empty() {
+            return Err(String::from("`key` was empty"))
+        }
+
+        let index = match self.find(&key) {
+            /*Err(e) => return Err(
+                format!(
+                "An error occured when attempted to find() '{0}'. Most likely '{0}' was not found in the rule file.",
+                key
+            )),*/
+            Err(_) => {
+                self.english_sentence.push(key);
+                return Ok(())
+            }
+            Ok(v) => v
+        };
+
+        for i in index..self.rules.len() {
+            // Is there a better way to do this than this slice thing?
+            match &self.rules[i..i + 1] {
+                "=" => { self.parse_value(i + 1); break }
+                _   => {}
+            }
+        }
+
+        Ok(())
+    }
+}
+
